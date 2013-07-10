5b3c06670696de9a98ff4ea72b836b4a7917a68a
Erran Carey <me@errancarey.com>
2013-07-10 12:10:34 -0500

    Add attributes/default

---
+++ attributes/default.rb
@@ -0,0 +1,5 @@
+# Example cookbook
+# Attributes: default
+
+default[:example][:title] = 'Pretty cool example huh?'
+default[:example][:body] = '<p>The quick brown fox jumped over the lazy brown dog.</p>'
